2f21055d0626a72e2d9116cdd5c8ada76198708f
jean-sebastien beaulieu <jeansebastienbeaulieuscrde.01gmail.com>
2025-03-06 22:46:03 -0500

    Enhance FindXCFun.cmake to include additional search paths and library targets

--- phi_framework/FindXCFun.cmake
+++ phi_framework/FindXCFun.cmake
@@ -1,11 +1,25 @@
 find_path(XCFun_INCLUDE_DIR
     NAMES xcfun.h
-    PATHS ${CMAKE_BINARY_DIR}/external/include
+    PATHS 
+        ${CMAKE_BINARY_DIR}/external/include
+        ${CMAKE_INSTALL_PREFIX}/include
+        $ENV{XCFUN_ROOT}/include
+        $ENV{PROGRAMFILES}/XCFun/include
+        $ENV{PROGRAMFILES\(X86\)}/XCFun/include
+        $ENV{USERPROFILE}/AppData/Local/Programs/XCFun/include
+    PATH_SUFFIXES xcfun
 )
 
 find_library(XCFun_LIBRARY
-    NAMES xcfun
-    PATHS ${CMAKE_BINARY_DIR}/external/lib
+    NAMES xcfun libxcfun
+    PATHS 
+        ${CMAKE_BINARY_DIR}/external/lib
+        ${CMAKE_INSTALL_PREFIX}/lib
+        $ENV{XCFUN_ROOT}/lib
+        $ENV{PROGRAMFILES}/XCFun/lib
+        $ENV{PROGRAMFILES\(X86\)}/XCFun/lib
+        $ENV{USERPROFILE}/AppData/Local/Programs/XCFun/lib
+    PATH_SUFFIXES lib lib64
 )
 
 include(FindPackageHandleStandardArgs)
@@ -16,4 +30,12 @@ find_package_handle_standard_args(XCFun
 if(XCFun_FOUND)
     set(XCFun_LIBRARIES ${XCFun_LIBRARY})
     set(XCFun_INCLUDE_DIRS ${XCFun_INCLUDE_DIR})
+    
+    if(NOT TARGET XCFun::XCFun)
+        add_library(XCFun::XCFun UNKNOWN IMPORTED)
+        set_target_properties(XCFun::XCFun PROPERTIES
+            IMPORTED_LOCATION "${XCFun_LIBRARY}"
+            INTERFACE_INCLUDE_DIRECTORIES "${XCFun_INCLUDE_DIR}"
+        )
+    endif()
 endif()
